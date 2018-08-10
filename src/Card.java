0c15ff8f16ce386cc344a3569fcf4503634e9804
Mohemmed Fayas <mohemmedfayas.m@gmail.com>
2018-08-10 15:03:30 +0530

    First Commit

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+public class Card {
+    public String suit;
+    public String rank;
+    public String color;
+
+    public Card( String color, String suit, String rank){
+        this.suit = suit;
+        this.rank = rank;
+        this.color = color;
+    }
+
+}
---
+++ src/Shoe.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Shoe {
+
+    private final String[][] SUITS = {{"Clubs", "Black"}, {"Spades", "Black"}, {"Diamonds", "Red"}, {"Hearts", "Red"}};
+    private final String[] RANKS = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+    private int deckCount;
+    private ArrayList<Card> deck;
+
+    public Shoe(int deckCount){
+        this.deckCount = deckCount;
+
+        for( int i = 0; i < deckCount; i++ ){
+            deck.addAll(generateDeck());
+        }
+    }
+
+    public  ArrayList<Card > generateDeck(){
+        ArrayList<Card> deck = new ArrayList<Card>();
+        for( int i = 0; i < SUITS.length; i++){
+            for( int j = 0; j < RANKS.length;j++){
+                deck.add(new Card(SUITS[i][1], SUITS[i][0], RANKS[j]));
+            }
+        }
+        return deck;
+    }
+
+    public  Card randomCard(){
+        Random rand = new Random();
+
+        int randomIndex = rand.nextInt(deck.size());
+        Card randomCard = deck.get(randomIndex);
+        deck.remove(randomIndex);
+
+        return randomCard;
+    }
+
+    public ArrayList<Card> randomNCard(int n){
+        ArrayList<Card> randomCard = new ArrayList<Card>();
+
+        for( int i = 0; i < n; i++){
+            randomCard.add(randomCard());
+        }
+
+        return randomCard;
+    }
+
+
+
+
+
+}
