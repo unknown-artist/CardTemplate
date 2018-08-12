d2c73de0d2c59ce12169a1af30fb60f931476128
unknown-artist <amitagarwal.17081996@gmail.com>
2018-08-13 00:27:09 +0530

    Added a beautiful toString() functionality

--- src/Shoe.java
+++ src/Shoe.java
@@ -4,8 +4,10 @@ import java.util.Random;
 public class Shoe {
 
     private final String[] SUITS = {"Clubs", "Spades","Diamonds", "Hearts"};
+    private final String[] SUITS_UNICODE = {"\u2663", "\u2660", "\u2666", "\u2665"};
     private final String[] COLORS ={"Black", "Black", "Red", "Red"};
     private final String[] RANKS = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+    private final String[] RANKS_STRING = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     private final int[] VALUE = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     private int deckCount;
     public ArrayList<Card> shoeCards;
@@ -19,7 +21,7 @@ public class Shoe {
         }
         
         for( int i = 0; i < jokerCount; i++){
-            shoeCards.add( new Card("","","Joker", 0));
+            shoeCards.add( new Card("","","Joker", 0, "", ""));
         }
     }
 
@@ -27,7 +29,7 @@ public class Shoe {
         ArrayList<Card> shoeCards = new ArrayList<Card>();
         for( int i = 0; i < SUITS.length; i++){
             for( int j = 0; j < RANKS.length;j++){
-                shoeCards.add(new Card(COLORS[i], SUITS[i], RANKS[j], VALUE[j]));
+                shoeCards.add(new Card(COLORS[i], SUITS[i], RANKS[j], VALUE[j], SUITS_UNICODE[i], RANKS_STRING[j]));
             }
         }
         return shoeCards;
